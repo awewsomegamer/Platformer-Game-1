6ddea655b21b7577e8fbe5829addf80b2222f40c
awewsomegamer <>
2020-07-08 17:59:57 -0700

    Created methods inside of renderer class

--- PlatformerGame/src/net/awewsomegaming/game/render/Renderer.java
+++ PlatformerGame/src/net/awewsomegaming/game/render/Renderer.java
@@ -3,5 +3,16 @@ package net.awewsomegaming.game.render;
 import javax.swing.JPanel;
 
 public class Renderer extends JPanel{
-
+	public void init() {
+		
+	}
+	public void render() {
+		
+	}
+	public void renderMap() {
+		
+	}
+	public void renderPlayer() {
+		
+	}
 }
