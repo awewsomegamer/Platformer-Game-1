3ba5b14e48258b48e8ecf68505ad0fd19df9fc9a
awewsomegamer <>
2020-07-08 17:43:13 -0700

    Created main class

---
+++ PlatformerGame/src/net/awewsomegaming/game/main/Main.java
@@ -0,0 +1,12 @@
+package net.awewsomegaming.game.main;
+
+import java.awt.Canvas;
+
+public class Main extends Canvas implements Runnable{
+	public void run() {
+		
+	}
+	public static void main(String args[]) {
+		
+	}
+}
